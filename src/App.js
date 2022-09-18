aceeb727cca4058bfa0b2d69c9f3f52f0d571dd4
Karim Omer <karimj991@gmail.com>
2022-09-18 22:55:32 +0200

    feat. add carousel to details page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { render } from 'react-dom';
-
-import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <BrowserRouter>
-  <header>
-    <Link to="/">Adopt Me!</Link>
-  </header>;
-  <Routes>
-    <Route path="/details/:id" element={<Details />} />
-    <Route path="/" element={<SearchParams />} />
-  </Routes>
-</BrowserRouter>
-)}
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
 
 render(<App />, document.getElementById("root"));
