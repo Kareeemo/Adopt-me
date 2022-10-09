1b34617e93acda3f3942cc576e5450b6a6cf9454
Karim Omer <karimj991@gmail.com>
2022-10-09 22:31:57 +0200

    Added themes

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
-
 import useBreedList from "./useBreedList";
 import Results from "./Results";
+import { useState, useEffect, useContext } from "react";
+import ThemeContext from "./ThemeContext";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
+  const [theme, setTheme] = useContext(ThemeContext);
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
@@ -81,7 +82,18 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <button>Submit</button>
+        <label htmlFor="theme">Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}>
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>;
+        <button style={{ backgroundColor: theme }}>Submit</button>;
       </form>
       <Results pets={pets} />
     </div>
